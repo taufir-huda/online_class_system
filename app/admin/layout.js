ef2d303a65af673637c69a1df05da11a7a1c40d5
taufir-huda <110535361+taufir-huda@users.noreply.github.com>
2023-11-08 17:10:29 +0600

    Courses page 3 ta ready

---
+++ app/admin/layout.js
@@ -0,0 +1,46 @@
+"use client";
+import "./data-tables-css.css";
+import "./satoshi.css";
+import { useState, useEffect } from "react";
+import Loader from "./components/common/Loader";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+
+export default function RootLayout({children}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+    setSidebarOpen(true)
+  }, []);
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="flex h-screen overflow-hidden">
+              <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+
+                <Header sidebarOpen={sidebarOpen}  setSidebarOpen={setSidebarOpen} />
+
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                    {children}
+                  </div>
+                </main>
+
+              </div>
+            </div>
+          )}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/admin/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div  className='mx-3'>
+      hvbjbb
+    </div>
+  )
+}
+
+export default page
