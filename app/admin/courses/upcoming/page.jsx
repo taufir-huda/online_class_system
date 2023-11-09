3f94bfd239ff1c5908c21337d49422d247fc8afc
taufir-huda <110535361+taufir-huda@users.noreply.github.com>
2023-11-09 18:26:42 +0600

    Student page is created

--- app/admin/courses/upcoming/page.jsx
+++ app/admin/courses/upcoming/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 function page() {
+  const test =[1,2,3,4,5,6,7,8,9]
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -20,24 +21,27 @@ function page() {
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-black p-6 rounded-lg text-bodydark2">
-              <Image className="h-40 rounded w-full object-cover object-center mb-6"
-              src={"/images/720x400.png"} alt="content" width={720}
-              height={400}
-              />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                SUBTITLE
-              </h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                Chichen Itza
-              </h2>
-              <p className="leading-relaxed text-base">
-                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                hexagon disrupt edison bulbche.
-              </p>
-            </div>
-          </div>
+          {test.map(()=>{
+            return(
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+             <div className="bg-black p-6 rounded-lg text-bodydark2">
+               <Image className="h-40 rounded w-full object-cover object-center mb-6"
+               src={"/images/720x400.png"} alt="content" width={720}
+               height={400}
+               />
+               <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                 SUBTITLE
+               </h3>
+               <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                 Chichen Itza
+               </h2>
+               <p className="leading-relaxed text-base">
+                 Fingerstache flexitarian street art 8-bit waistcoat. Distillery
+                 hexagon disrupt edison bulbche.
+               </p>
+             </div>
+            </div>)
+          })}
         </div>
       </div>
     </section>
---
+++ app/admin/students/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableOne from '../components/Tables/TableOne'
+
+function page() {
+  return (
+    <div className="col-span-12 xl:col-span-8">
+        <TableOne />
+    </div>
+  )
+}
+
+export default page
