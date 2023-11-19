5a2533b71f70a70ae81b52e8f6fc125b7af0d16b
taufir-huda <110535361+taufir-huda@users.noreply.github.com>
2023-11-20 01:22:13 +0600

    student details page ta halka ready

--- app/admin/components/Tables/TableThree.jsx
+++ app/admin/components/Tables/TableThree.jsx
@@ -29,6 +29,9 @@ const TableThree = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
+        <h4 className="text-title-sm2 font-bold text-black dark:text-white">
+          Courses Registered
+        </h4>
         <table className="w-full table-auto">
           <thead>
             <tr className="bg-gray-2 text-left dark:bg-meta-4">
