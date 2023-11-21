344cb860b12c764dc104aa3cb5a44e7b9a2fa666
Irtisam Sajin <irtisamcoding@gmail.com>
2023-11-21 12:46:03 +0600

    Added institution and student id fields to the singnup/login forms

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -14,17 +14,17 @@ export default function Example() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
-                htmlFor="email"
+                htmlFor="studentId"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Email address
+                Student ID
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="studentId"
+                  name="studentId"
+                  type="number"
+                  autoComplete="studentId"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
