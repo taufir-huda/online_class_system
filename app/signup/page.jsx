69001d77ad12429e86676c30443be6c3f8ea3d7c
Irtisam Sajin <irtisamcoding@gmail.com>
2023-11-22 22:52:36 +0600

    Added states to Login and Submit pages

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,4 +1,24 @@
+"use client"
+
+import { useState } from "react";
+
+
 export default function Example() {
+
+  const [user,setUser]=useState({
+    name: "",
+    institution: "",
+    email:"",
+    phone:"",
+    pass:"",
+    confirmPass:"",
+  });
+
+  const onSubmit= async (e) => {
+    e.preventDefault();
+    console.log(user);
+  }
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -24,6 +44,8 @@ export default function Example() {
                   type="text"
                   required
                   autoComplete="name"
+                  value={user.name}
+                  onChange={(e) => {setUser({...user,name:e.target.value})}}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -42,6 +64,8 @@ export default function Example() {
                   type="text"
                   required
                   autoComplete="institution"
+                  value={user.institution}
+                  onChange={(e) => {setUser({...user,institution:e.target.value})}}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -59,6 +83,8 @@ export default function Example() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={user.email}
+                  onChange={(e) => {setUser({...user,email:e.target.value})}}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -77,6 +103,8 @@ export default function Example() {
                   name="phone"
                   type="tel"
                   autoComplete="phone"
+                  value={user.phone}
+                  onChange={(e) => {setUser({...user,phone:e.target.value})}}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -96,6 +124,8 @@ export default function Example() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  value={user.pass}
+                  onChange={(e) => {setUser({...user,pass:e.target.value})}}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -114,6 +144,8 @@ export default function Example() {
                   name="confirmPassword"
                   type="password"
                   autoComplete="confirmPassword"
+                  value={user.confirmPass}
+                  onChange={(e) => {setUser({...user,confirmPass:e.target.value})}}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -123,6 +155,7 @@ export default function Example() {
             <div>
               <button
                 type="submit"
+                onClick={onSubmit}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Signup
